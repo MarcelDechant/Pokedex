83a6f8cac5f74d8b0315b8eefff640e5bc5e4554
Marcel Dechant <marcel.dechant@freenet.de>
2024-01-18 20:52:52 +0100

    type colors

--- templates.js
+++ templates.js
@@ -1,13 +1,13 @@
 function createCard(j) {
     document.getElementById('allPokemons').innerHTML +=/*html*/ ` 
     <div class="pokecard">
-        <div id="pokedex">
+        <div class="pokedex" id="pokedex${j}">
             <div class="poke-num-name">
                 <h2 class="nMargin" id="pokeNumber${j}">#</h2>
                 <h1 class="nMargin" id="pokeName${j}">Name</h1>
             </div>
             <div class="typeAndImage">
-                <div class="type" id="type${j}"></div>
+                <p class="type" id="type${j}"></p>
                 <img class="pokeimg" id="pokeimg${j}">
                 <img class="PokeballTranzparent" src="img/Pokeball-tranzparent.png" alt="">
             </div>
